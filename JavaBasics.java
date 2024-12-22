a08909325030223a30cc31df60645b78f21e5667
KasavRushikesh09 <rishikeshkasav2003@gmail.com>
2024-12-22 10:31:40 +0530

    add something

--- JavaBasics.java
+++ JavaBasics.java
@@ -28,14 +28,66 @@
 
 //Question (3) :
 
-import java.util.*;
-public class JavaBasics{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        float pencil = sc.nextFloat();
-        float pen    = sc.nextFloat();
-        float eraser = sc.nextFloat();
-        float Total_cost = (pencil+pen+eraser)+((pencil+pen+eraser)*18)/100;
-        System.out.println("Bill with 18% tax : " + Total_cost);
+// import java.util.*;
+// public class JavaBasics{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         float pencil = sc.nextFloat();
+//         float pen    = sc.nextFloat();
+//         float eraser = sc.nextFloat();
+//         float Total_cost = (pencil+pen+eraser)+((pencil+pen+eraser)*18)/100;
+//         System.out.println("Bill with 18% tax : " + Total_cost);
+//     }
+// }
+
+// Arithmetic operators
+
+// import java.util.*;
+// public class JavaBasics{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         int A = 10;
+//         int B = 5;
+//         System.out.println("Addition =" + (A+B));
+//         System.out.println("Substraction =" + (A-B));
+//         System.out.println("Multiplication =" + (A*B));
+//         System.out.println("Division =" + (A/B));
+//         System.out.println("Modulo =" + (A%B));
+//     }
+// }
+
+
+// Relational Operator
+
+// import java.util.*;
+// public class JavaBasics{
+//     public static void main(String args[]){
+//       Scanner sc = new Scanner(System.in);
+//       int a = 10;
+//       int b = 5;
+//       System.out.println(a >= b);
+//     }
+// }
+
+// import java.util.*;
+// public class JavaBascis{
+//     public static void main(String args []){
+//         Scanner sc = new Scanner(System.in);
+//    int x,y,z;
+//    x=y=z=2;
+//    x += y;
+//    y -=z;
+//    z /= (x+y);
+//    System.out.println(x +" "+y+" "+z);
+//     }
+// }
+
+public class JavaBascis{
+    public static void main(String []args) {
+        int x = 9, y = 12;
+        int a = 2 ,b = 4 , c= 6;
+         
+         int exp = 4/3 * (x + 34) + 9 * (a + b + c) +(3 + y * (2+a)) / (a + b *y);
+         System.out.println(exp);
     }
 }
