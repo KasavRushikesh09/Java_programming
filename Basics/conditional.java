cf28b037f625f28bbdbd55cc09a547de846f54bb
KasavRushikesh09 <rishikeshkasav2003@gmail.com>
2024-12-26 11:24:08 +0530

    add

--- Basics/conditional.java
+++ Basics/conditional.java
@@ -87,7 +87,7 @@
 // }
 
 import java.util.*;
-public class conditioanl{
+public class conditional{
     public static void main(String args []){
         Scanner sc = new Scanner(System.in);
         int income = sc.nextInt();
