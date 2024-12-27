2a6a5f046e40b27fe3683106bdc12af8e3449de0
KasavRushikesh09 <rishikeshkasav2003@gmail.com>
2024-12-27 23:05:23 +0530

    add 1 file

---
+++ Arrays/pairsInArray.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class pairsInArray{
+    public static void pair_array(int number[]){
+     for(int i=0; i<number.length; i++){
+       int curr = number[i];
+       for(int j=i+1; j<number.length; j++){
+        System.out.print("(" + curr + "," + number[j] + ")");
+       }
+       System.out.println();
+     }
+    }
+    public static void main(String args[]){
+      int number[] = {2,4,6,8,10};
+      pair_array(number);
+    }
+}
