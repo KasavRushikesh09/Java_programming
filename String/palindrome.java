827963d1794bf749d3cda9d57ac9ab1ae76534b1
KasavRushikesh09 <rishikeshkasav2003@gmail.com>
2024-12-30 11:41:11 +0530

    Add palindrome exaple

---
+++ String/palindrome.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class palindrome{
+    public static boolean isPalindrome(String str){
+       int n = str.length();
+       for(int i=0; i<n/2; i++){
+        if(str.charAt(0) != str.charAt(n-1-i)){
+            return false;
+        }
+       }return true;
+    }
+    public static void main(String args[]){
+       Scanner sc = new Scanner(System.in);
+       System.out.println("value of string: ");
+       String str = sc.nextLine();
+       System.out.println(isPalindrome(str));
+    }
+}
